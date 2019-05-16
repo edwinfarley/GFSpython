3fa8a471fb6b62f6aedd9c37569ce62e152ec9a4
Edwin <edwin_farley@brown.edu>
2019-05-16 13:13:31 -0400

    05/16/2019

--- perm_sample_norm_08.py
+++ perm_sample_norm_08.py
@@ -38,7 +38,6 @@ def simulate_data_normal(N, B):
     # Create a pandas DataFrame with column 'x' containing
     # N uniformly sampled values between 0.0 and 1.0
     seed = 7
-    print(1)
     df = pd.DataFrame(
         {str("x1"): np.random.RandomState().normal(
                 0, v, N)})
@@ -59,11 +58,11 @@ def simulate_data_normal(N, B):
     return df
 
 
-def normal_permute(A, b, y, p, T, m, sd, Y):
+def normal_permute(A, b, y, p, T, m, Y):
     #Performs the Metropolis-Hastings step.
     
     #T number of iterations
-    for t in range(T):
+    for t in range(T * m):
         i, j = np.random.choice(m, 2, replace = False)
         
         x_i = np.matmul(A[i, :], b)
@@ -79,10 +78,10 @@ def normal_permute(A, b, y, p, T, m, sd, Y):
         
         #Calculate log-likelihoods (terms that cancel are not included)
         #New likelihood with swapped values
-        new_l = sum([np.log(l) for l in norm.pdf([x_i_swap, x_j_swap], [y[j], y[i]], sd)\
+        new_l = sum([np.log(l) for l in norm.pdf([x_i_swap, x_j_swap], [y[j], y[i]], 1)\
                      *(np.isfinite([y[j], y[i]]))])
         #Likelihood without swapped values
-        old_l = sum([np.log(l) for l in norm.pdf([x_i, x_j], [y[i], y[j]], sd)\
+        old_l = sum([np.log(l) for l in norm.pdf([x_i, x_j], [y[i], y[j]], 1)\
                      *(np.isfinite([y[i], y[j]]))])
         
         #Probability of accepting proposed swap from definition of Metropolis-Hastings
@@ -107,7 +106,7 @@ def normal_permute(A, b, y, p, T, m, sd, Y):
     #Returns final permutation of input and the permuted y
     return(p, y)
 
-def permute_search_normal(df, block, formula, Y, N, I, T, burnin, interval):
+def permute_search_normal(df, block, formula, Y, N, I, T, burnin, interval, t, P_t, b, block_size, original_block, X_missing, num_X_missing):
     #N: Number of permutations
     #I: Number of samples in sampling Betas
     #T: Number of iterations in row swapping phase
@@ -122,86 +121,75 @@ def permute_search_normal(df, block, formula, Y, N, I, T, burnin, interval):
     block_df = pd.DataFrame(df[block[0]:block[1]]).reset_index(drop=True)
 
     #Missing values: Find indices of missing values and how many there are.
-    X_missing = np.where(np.isnan(block_df[covariates[0]]))[0]
-    num_X_missing = len(X_missing)
+    if t == 0:
+        X_missing = np.where(np.isnan(block_df[covariates[0]]))[0]
+        num_X_missing = len(X_missing)
+        #Make a copy of the block as it was given in the input.
+        original_block = pd.DataFrame(block_df)
+        block_size = sum(block_df[covariates[0]].notnull())
     num_finite = len(block_df) - num_X_missing
-    num_Y_missing = sum(np.isnan(block_df[y1]))
+    num_Y_missing = sum(np.int_(np.isnan(block_df[y1])))
     m, n = len(block_df), len(block_df.columns)+1
     
     #Remove NaNs outside of current block
-    df = pd.concat([df[0:block[0]].dropna(), df[block[0]:block[1]], df[block[1]:].dropna()])
-    block_size = sum(block_df[covariates[0]].notnull())
-    print(block_size)
+    #df = pd.concat([df[0:block[0]].dropna(), df[block[0]:block[1]], df[block[1]:].dropna()])
+    #print(block_size)
     #P: Permutations after I iterations for each set of Betas
     P = np.zeros((N, block_size)).astype(int)
     
     #B: Betas for T samplings
     B = [0 for i in range(n)]*N
     
-    #Make a copy of the block as it was given in the input.
-    original_block = pd.DataFrame(block_df)
     #Sample missing X's
-    for i in X_missing:
-                r = int(num_finite * random.random())
-                #print((block_df.sort_values(by = y1).drop(y1, 1))[r:r+1])
-                #print(block_df.loc[i, :][:num_X])
-                block_df.loc[i, :][:num_X] = list((block_df.sort_values(by = y1).drop(y1, 1)).loc[r,:][:num_X])
-    
-    for t in range(burnin + (N*interval)):
-        #Input is the data in the order of the last permutation
-        if t > 0:
-            #Update block_df with the new permutation of y
-            df[y1].loc[block[0]:block[1]-1] = new_y
-            block_df[y1] = new_y
-            #Update columns from Y that correspond to covariates that need to be permuted.
-            for col, _ in Y:
-                new_col = build_permutation(P_t, list(original_block[col]))
-                df[col].loc[block[0]:block[1]-1] = new_col
-                block_df[col] = new_col
-            #Sample missing X's from response values estimated with most recent Betas.
-            if num_X_missing:
-                block_df['y_b'] = np.matmul(A, b)
-                for i in X_missing:
+    if t == 0:
+        for i in X_missing:
                     r = int(num_finite * random.random())
-                    block_df.loc[i, :][:num_X] = list((block_df.sort_values(by = 'y_b').drop([y1, 'y_b'], 1)).loc[r,:][:num_X])
-                block_df = block_df.drop('y_b', 1)
-        
-        #Sample Betas and search for permutations
-        trace = glm_mcmc_inference(df, formula, pm.glm.families.Normal(), I)
-        beta_names = ['Intercept']
-        beta_names.extend(formula.split(' ~ ')[1].split(' + '))
-        b = np.transpose([trace.get_values(s)[-1] for s in beta_names])
-        sd = trace.get_values('sd')[-1]
-        
+                    #print((block_df.sort_values(by = y1).drop(y1, 1))[r:r+1])
+                    #print(block_df.loc[i, :][:num_X])
+                    block_df.loc[i, :][:num_X] = list((block_df.sort_values(by = y1).drop(y1, 1)).loc[r,:][:num_X])
+    
+    #for t in range(burnin + (N*interval)):
+    #Input is the data in the order of the last permutation
+    #Sample a single permutation
+
+    #print(block, "\n", block_df)
+    if block_size > 1:
         A = pd.DataFrame.as_matrix(block_df.drop(y1, 1))
         A = np.concatenate([np.ones((m, 1)), A], 1)
-        B[((t)*n):((t+1)*n)] = b
-        
+        B = b
         #Go to Metropolis-Hastings step.
         if t == 0:
-            P_t, new_y = normal_permute(A, b, np.array(block_df[y1]), np.arange(0, m), T, m, sd, Y)
-            #Save permutation
-            if burnin == 0:
-                if num_X_missing:
-                    P[0, :] = P_t[:-num_X_missing]
-                elif num_Y_missing:
-                    temp = np.array(P_t)
-                    temp[np.where(np.isnan(new_y))] = -(block[0]+1)
-                    P[0, :] = temp
-                else:
-                    P[0, :] = P_t   
+            P_t, new_y = normal_permute(A, b, np.array(block_df[y1]), np.arange(0, m), T, m, Y)
         else:
-            P_t, new_y = normal_permute(A, b, np.array(new_y), P_t, T, m, sd, Y)
-            #Save permutation
-            if t >= burnin:
-                if (t-burnin)%interval == 0:
-                    if num_X_missing:
-                        P[int((t-burnin)/interval), :] = P_t[:-num_X_missing]
-                    elif num_Y_missing:
-                        temp = np.array(P_t)
-                        temp[np.where(np.isnan(new_y))] = -(block[0]+1)
-                        P[int((t-burnin)/interval), :] = temp
-                    else:
-                        P[int((t-burnin)/interval), :] = P_t 
+            P_t, new_y = normal_permute(A, b, np.array(block_df[y1]), P_t, T, m, Y)
 
-    return([B, P])
+        if num_X_missing:
+            P = P_t[:-num_X_missing]
+        elif num_Y_missing:
+            temp = np.array(P_t)
+            temp[np.where(np.isnan(new_y))] = -(block[0]+1)
+            P = temp
+        else:
+            P = P_t
+
+        #Update block_df with the new permutation of y
+        df[y1].loc[block[0]:block[1]-1] = new_y
+        block_df[y1] = new_y
+        #Update columns from Y that correspond to covariates that need to be permuted.
+        for col, _ in Y:
+            new_col = build_permutation(P_t, list(original_block[col]))
+            df[col].loc[block[0]:block[1]-1] = new_col
+            block_df[col] = new_col
+            #Sample missing X's from response values estimated with most recent Betas.
+        if num_X_missing:
+            block_df['y_b'] = np.matmul(A, b)
+            for i in X_missing:
+                r = int(num_finite * random.random())
+                block_df.loc[i, :][:num_X] = list((block_df.sort_values(by = 'y_b').drop([y1, 'y_b'], 1)).loc[r,:][:num_X])
+            block_df = block_df.drop('y_b', 1)
+    else:
+        P = [0]
+    if t == 0:
+        return([B, P, P_t, block_df, block_size, original_block, X_missing, num_X_missing])
+    else:
+        return([B, P, P_t, block_df])
